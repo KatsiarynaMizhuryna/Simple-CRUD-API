94a173b4f00d8ba504544211a543ad826eebe9b8
Kate <ekaterina21051988@yandex.by>
2024-02-12 17:21:27 +0200

    feat: routers added

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,22 @@ import http from "http";
 import { getAllUsers } from "./methods/get_all_persons";
 import { createNewUser } from "./methods/create_new_user";
 import { getUserById } from "./methods/get_user_by_id";
+import { deleteUserById } from "./methods/delete_user";
+import { updateUserById } from "./methods/update_existing_user";
 
 const users: User[] = [];
 
 const server = http.createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
     getAllUsers(res, users);
+  } else if (req.url!.startsWith("/api/users/") && req.method === "GET") {
+    getUserById(req, res, users);
   } else if (req.url === "/api/users" && req.method === "POST") {
     createNewUser(res, req, users);
-  } else if (req.url!.startsWith("/api/users/") && req.method === "GET") {
-    const userId: any = req.url!.split("/").pop();
-    getUserById(req, res, users, userId);
+  } else if (req.url!.startsWith("/api/users/") && req.method === "PUT") {
+    updateUserById(req, res, users);
+  } else if (req.url!.startsWith("/api/users/") && req.method === "DELETE") {
+    deleteUserById(req, res, users);
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Not Found" }));
